fd71db509aaac52c20cf2070fb0bb3a07c31e4cf
diana <dianakieru1@gmil.com>
2022-05-31 21:10:38 +0300

    added classes

---
+++ app/src/main/java/com/moringaschool/weatherapi/models/Weather.java
@@ -0,0 +1,79 @@
+package com.moringaschool.weatherapi.models;
+
+
+
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+
+public class Weather {
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("main")
+    @Expose
+    private String main;
+    @SerializedName("description")
+    @Expose
+    private String description;
+    @SerializedName("icon")
+    @Expose
+    private String icon;
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public Weather() {
+    }
+
+    /**
+     * 
+     * @param icon
+     * @param description
+     * @param main
+     * @param id
+     */
+    public Weather(Integer id, String main, String description, String icon) {
+        super();
+        this.id = id;
+        this.main = main;
+        this.description = description;
+        this.icon = icon;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMain() {
+        return main;
+    }
+
+    public void setMain(String main) {
+        this.main = main;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+}
