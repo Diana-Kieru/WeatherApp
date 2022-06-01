8e9c5d96851792c313b58b063df1fccd96f71c42
diana <dianakieru1@gmil.com>
2022-06-01 15:54:27 +0300

    added mainactivity contents

--- app/src/main/java/com/moringaschool/weatherapi/MainActivity.java
+++ app/src/main/java/com/moringaschool/weatherapi/MainActivity.java
@@ -3,12 +3,41 @@ package com.moringaschool.weatherapi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.moringaschool.weatherapi.models.WeatherResponse;
+import com.moringaschool.weatherapi.network.WeatherApi;
+import com.moringaschool.weatherapi.network.WeatherClient;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WeatherApi weatherApiClient = WeatherClient.getClient();
+        Call<WeatherResponse> call = weatherApiClient.getCurrentWeather("Nairobi");
+        call.enqueue(new Callback<WeatherResponse>() {
+            @Override
+            public void onResponse(Call<WeatherResponse> call, Response<WeatherResponse> response) {
+                if (response.isSuccessful()){
+                    Log.d(TAG, response.body().getName());
+                }else {
+                    Log.e(TAG, "Error");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<WeatherResponse> call, Throwable t) {
+                Log.e("Error Message", "onFailure: ",t );
+
+            }
+        });
     }
 }
--- app/src/main/java/com/moringaschool/weatherapi/network/WeatherClient.java
+++ app/src/main/java/com/moringaschool/weatherapi/network/WeatherClient.java
@@ -1,5 +1,19 @@
 package com.moringaschool.weatherapi.network;
 
-public class WeatherClient {
+import com.moringaschool.weatherapi.Constants;
 
+import okhttp3.HttpUrl;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class WeatherClient {
+        private static Retrofit retrofit = null;
+        public static WeatherApi getClient(){
+            if (retrofit == null){
+                HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.WEATHER_BASE_URL).newBuilder();
+                urlBuilder.addQueryParameter(Constants.API_KEY_QUERY_PARAMETER,Constants.WEATHER_API_KEY);
+                retrofit = new Retrofit.Builder().baseUrl(urlBuilder.build().toString()).addConverterFactory(GsonConverterFactory.create()).build();
+            }
+            return retrofit.create(WeatherApi.class);
+        }
 }
