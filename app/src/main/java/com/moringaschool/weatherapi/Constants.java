e543460f559f8181e1ca63159505f69d1ebbb97e
diana <dianakieru1@gmil.com>
2022-06-01 15:10:41 +0300

    added API

--- app/src/main/java/com/moringaschool/weatherapi/Constants.java
+++ app/src/main/java/com/moringaschool/weatherapi/Constants.java
@@ -1,6 +1,6 @@
 package com.moringaschool.weatherapi;
 
-public class constants {
+public class Constants {
     public static final String WEATHER_BASE_URL = "https://api.openweathermap.org/data/2.5";
     public static final String WEATHER_API_KEY = BuildConfig.WEATHER_API_KEY;
 }
