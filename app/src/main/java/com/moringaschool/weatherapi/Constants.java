cf98d234d1a2f6956fd3decefe7fec06ea0b99f1
diana <dianakieru1@gmil.com>
2022-06-01 15:26:33 +0300

    added client content

--- app/src/main/java/com/moringaschool/weatherapi/Constants.java
+++ app/src/main/java/com/moringaschool/weatherapi/Constants.java
@@ -3,4 +3,5 @@ package com.moringaschool.weatherapi;
 public class Constants {
     public static final String WEATHER_BASE_URL = "https://api.openweathermap.org/data/2.5";
     public static final String WEATHER_API_KEY = BuildConfig.WEATHER_API_KEY;
+    public static final String API_KEY_QUERY_PARAMETER = "appid";
 }
--- app/src/main/java/com/moringaschool/weatherapi/network/WeatherClient.java
+++ app/src/main/java/com/moringaschool/weatherapi/network/WeatherClient.java
@@ -1,4 +1,5 @@
 package com.moringaschool.weatherapi.network;
 
 public class WeatherClient {
+
 }
