bca566d613fe375632e2c9ba36a05b3b5fc8c5b9
diana <dianakieru1@gmil.com>
2022-06-01 12:59:08 +0300

    added network

--- app/src/main/java/com/moringaschool/weatherapi/models/WeatherResponse.java
+++ app/src/main/java/com/moringaschool/weatherapi/models/WeatherResponse.java
@@ -1,14 +1,13 @@
 package com.moringaschool.weatherapi.models;
-​
+
 import java.util.List;
-​
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
-​
-        ​
-        ​
+
+
 public class WeatherResponse {
-​
+
     @SerializedName("coord")
     @Expose
     private Coord coord;
@@ -48,14 +47,14 @@ public class WeatherResponse {
     @SerializedName("cod")
     @Expose
     private Integer cod;
-​
+
     /**
      * No args constructor for use in serialization
      *
      */
     public WeatherResponse() {
     }
-​
+
     /**
      *
      * @param visibility
@@ -88,109 +87,109 @@ public class WeatherResponse {
         this.name = name;
         this.cod = cod;
     }
-​
+
     public Coord getCoord() {
         return coord;
     }
-​
+
     public void setCoord(Coord coord) {
         this.coord = coord;
     }
-​
+
     public List<Weather> getWeather() {
         return weather;
     }
-​
+
     public void setWeather(List<Weather> weather) {
         this.weather = weather;
     }
-​
+
     public String getBase() {
         return base;
     }
-​
+
     public void setBase(String base) {
         this.base = base;
     }
-​
+
     public Main getMain() {
         return main;
     }
-​
+
     public void setMain(Main main) {
         this.main = main;
     }
-​
+
     public Integer getVisibility() {
         return visibility;
     }
-​
+
     public void setVisibility(Integer visibility) {
         this.visibility = visibility;
     }
-​
+
     public Wind getWind() {
         return wind;
     }
-​
+
     public void setWind(Wind wind) {
         this.wind = wind;
     }
-​
+
     public Clouds getClouds() {
         return clouds;
     }
-​
+
     public void setClouds(Clouds clouds) {
         this.clouds = clouds;
     }
-​
+
     public Integer getDt() {
         return dt;
     }
-​
+
     public void setDt(Integer dt) {
         this.dt = dt;
     }
-​
+
     public Sys getSys() {
         return sys;
     }
-​
+
     public void setSys(Sys sys) {
         this.sys = sys;
     }
-​
+
     public Integer getTimezone() {
         return timezone;
     }
-​
+
     public void setTimezone(Integer timezone) {
         this.timezone = timezone;
     }
-​
+
     public Integer getId() {
         return id;
     }
-​
+
     public void setId(Integer id) {
         this.id = id;
     }
-​
+
     public String getName() {
         return name;
     }
-​
+
     public void setName(String name) {
         this.name = name;
     }
-​
+
     public Integer getCod() {
         return cod;
     }
-​
+
     public void setCod(Integer cod) {
         this.cod = cod;
     }
-​
+
 }
--- app/src/main/java/com/moringaschool/weatherapi/network/WeatherApi.java
+++ app/src/main/java/com/moringaschool/weatherapi/network/WeatherApi.java
@@ -5,8 +5,9 @@ import com.moringaschool.weatherapi.models.WeatherResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface WeatherApi {
     @GET("/weather")
-    Call<WeatherResponse> getCurrentWeather()
+    Call<WeatherResponse> getCurrentWeather(@Query("q") String city);
 }
